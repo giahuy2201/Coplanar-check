1c3a5ea4f82f42c1b4e3019ec94afa6f8c371108
Gia Huy Nguyen <giahuy2201@.outlook.com>
2018-02-28 10:48:00 -0500

    Update some comments

--- coplanar.cpp
+++ coplanar.cpp
@@ -1,3 +1,8 @@
+/*
+ * By giahuy2201
+ * Date: Feb 26, 2018
+ */
+
 #include <iostream>
 
 int match(int *icoor, int *coor);
@@ -5,22 +10,22 @@ int match(int *icoor, int *coor);
 
 int main()
 {
-    int *icoor = new int[6];
+    int *icoor = new int[6]; // to store 2 inital point
     std::cout << "Enter intial points: ";
-    for(int i=0; i< 6;i++)
+    for(int i=0; i< 6;i++) // read 6 numbers one by one
     {
         std::cin >> icoor[i];
     }
 
-    while(true)
+    while(true) // let user enter the third number to check (for multiple-choice test :D)
     {
-        int *coor = new int[3];
+        int *coor = new int[3]; // to store the point
         std::cout << "\n\nType a point: ";
         for(int i=0; i< 3;i++)
         {
             std::cin >> coor[i];
         }
-        if(match(icoor, coor))
+        if(match(icoor, coor)) // if it fit two initial points -> DONE
         {
             std::cout << "-> TRUE";
         }
@@ -31,12 +36,16 @@ int main()
 
 int match(int *icoor, int *coor)
 {
-    float y = (float)(icoor[1]*coor[0] - coor[1]*icoor[0]) / (icoor[1]*icoor[3] - icoor[0]*icoor[4]);
+    /*
+     * to check if the third point with coordinates in coor array is fit the inital points.
+     */
+    float y = (float)(icoor[1]*coor[0] - coor[1]*icoor[0]) / (icoor[1]*icoor[3] - icoor[0]*icoor[4]); // solve the 2-unknown equation to get x and y
     float x = (coor[0] - icoor[3]*y) / icoor[0];
-    std::cout << "\nx: " << x << " y: " << y;
-    int c = icoor[2]*x + icoor[5]*y;
 
-    if(c == coor[2])
+    std::cout << "\nx: " << x << " y: " << y; // let user know the unknowns' value
+    int c = icoor[2]*x + icoor[5]*y; // calculate the expected result
+
+    if(c == coor[2]) // compare and DONE
     {
         return true;
     }
